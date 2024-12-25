4e9f2718731d476923a5164852e271e229922492
Vera Pervushina <vera.pervushina2608@gmail.com>
2024-12-25 22:56:59 +0300

    added start page

--- settings.js
+++ settings.js
@@ -1,1 +1,16 @@
+
+const GAME_TYPES = [
+  { type: 'cats', text: 'Котики' },
+  { type: 'flowers', text: 'Цветочки' },
+  { type: 'cars', text: 'Машины' },
+];
+
+const AppRoute = {
+  Initial: 'initial',
+  Game: 'game',
+  Results: 'results',
+};
+
 const TIMEOUT = 900;
+
+const RANDOMIZED = false;
